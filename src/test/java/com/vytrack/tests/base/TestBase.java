965b98b6d7d3068812a90bd6a3c6e5e47e63dd54
Muhammet <muhammedabuchary@gmail.com>
2022-03-12 12:02:47 -0600

    TestBase_update

--- src/test/java/com/vytrack/tests/base/TestBase.java
+++ src/test/java/com/vytrack/tests/base/TestBase.java
@@ -1,18 +1,23 @@
 package com.vytrack.tests.base;
 
 
+import com.vytrack.pages.DashboardPage;
+import com.vytrack.pages.VytrackLoginPage;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class TestBase {
-
-
+public abstract class TestBase {
+    public VytrackLoginPage loginPage;
+    public DashboardPage dashboardPage;
     @BeforeMethod
     public void setUp() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
+       // Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
         //called method from 'Driver' Utils and config.prop
+        System.out.println("Test is started");
+        loginPage = new VytrackLoginPage();
+        dashboardPage = new DashboardPage();
             }
 
     @AfterMethod
